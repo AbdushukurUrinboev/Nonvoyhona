ee7bd539e9c5691c6ff241575b17ea1a59b23243
AbdushukurUrinboev <urinboevabdushukur@gmail.com>
2023-04-09 16:52:06 +0500

    changed

--- src/API.js
+++ src/API.js
@@ -1,6 +1,4 @@
-export const base_URL = "http://127.0.0.1:4000"
-// export const base_URL = "http://localhost:4000"
-
+export const base_URL = "http://85.193.82.65:4000"
 
 export const CUSTOMERS_URL = base_URL + '/customers'; // tugri
 export const STORAGE_URL = `${base_URL}/storage`; // products -- tugirladim
